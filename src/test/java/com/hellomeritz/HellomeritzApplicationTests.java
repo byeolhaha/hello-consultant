5b745cbb48b04ec9f9ddce1c503c28182b084260
byeolhaha <j35635aa@gmail.com>
2024-04-01 21:41:28 +0900

    chore : 초기 설정

---
+++ src/test/java/com/hellomeritz/HellomeritzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellomeritz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellomeritzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
