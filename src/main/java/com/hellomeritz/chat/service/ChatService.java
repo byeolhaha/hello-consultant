8e46956809ea8eb05fb28348e8de599b3a94aefc
byeolhaha <j35635aa@gmail.com>
2024-04-10 18:31:14 +0900

    feat : 채팅방 생성 서비스 구현

--- src/main/java/com/hellomeritz/chat/service/ChatService.java
+++ src/main/java/com/hellomeritz/chat/service/ChatService.java
@@ -1,10 +1,14 @@
 package com.hellomeritz.chat.service;
 
+import com.hellomeritz.chat.domain.ChatRoom;
 import com.hellomeritz.chat.global.client.Translator;
 import com.hellomeritz.chat.global.client.TranslationResponse;
-import com.hellomeritz.chat.repository.ChatMessageRepository;
+import com.hellomeritz.chat.repository.chatmessage.ChatMessageRepository;
+import com.hellomeritz.chat.repository.chatroom.ChatRoomRepository;
 import com.hellomeritz.chat.service.dto.param.ChatMessageTextParam;
+import com.hellomeritz.chat.service.dto.param.ChatRoomCreateParam;
 import com.hellomeritz.chat.service.dto.result.ChatMessageTranslateTextResult;
+import com.hellomeritz.chat.service.dto.result.ChatRoomCreateResult;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,21 +16,33 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class ChatService {
     private final ChatMessageRepository chatMessageRepository;
-    private final Translator translater;
+    private final ChatRoomRepository chatRoomRepository;
+    private final Translator translator;
 
-    public ChatService(ChatMessageRepository chatMessageRepository, Translator translater) {
+    public ChatService(ChatMessageRepository chatMessageRepository, ChatRoomRepository chatRoomRepository, Translator translator) {
         this.chatMessageRepository = chatMessageRepository;
-        this.translater = translater;
+        this.chatRoomRepository = chatRoomRepository;
+        this.translator = translator;
     }
 
     @Transactional
     public ChatMessageTranslateTextResult translateText(ChatMessageTextParam param) {
-        TranslationResponse translatedResponse = translater.translate(param.toTranslationRequest());
+        TranslationResponse translatedResponse = translator.translate(param.toTranslationRequest());
 
         return ChatMessageTranslateTextResult.to(
             chatMessageRepository.save(param.toChatMessage()),
             chatMessageRepository.save(param.toChatMessage(translatedResponse.getText()))
         );
+    }
+
+    @Transactional
+    public ChatRoomCreateResult createChatRoom(ChatRoomCreateParam param) {
+        return ChatRoomCreateResult.to(
+            chatRoomRepository.save(
+                ChatRoom.of(param.fcId(), param.userId())
+            )
+        );
+
     }
 
 }
