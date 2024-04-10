bc6ed9e23312f021c1adfb645d90d0d35acf0d96
byeolhaha <j35635aa@gmail.com>
2024-04-10 14:04:29 +0900

    test : chat message 저장 서비스 테스트 구현

---
+++ src/test/java/com/hellomeritz/chat/service/ChatFixture.java
@@ -0,0 +1,18 @@
+package com.hellomeritz.chat.service;
+
+import com.hellomeritz.chat.service.dto.param.ChatMessageTextParam;
+
+public class ChatFixture {
+
+    private ChatFixture() {
+        throw new AssertionError("생성자를 통해서 인스턴스를 생성할 수 없습니다.");
+    }
+
+    public static ChatMessageTextParam chatMessageTextParam() {
+        return new ChatMessageTextParam(
+            "안녕하세요 반가워요",
+            1,
+            true
+        );
+    }
+}
---
+++ src/test/java/com/hellomeritz/chat/service/ChatServiceTest.java
@@ -0,0 +1,38 @@
+package com.hellomeritz.chat.service;
+
+import com.hellomeritz.chat.repository.ChatMessageRepository;
+import com.hellomeritz.chat.service.dto.param.ChatMessageTextParam;
+import com.hellomeritz.chat.service.dto.result.ChatMessageTranslateTextResult;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.NONE;
+
+@Transactional
+@SpringBootTest(webEnvironment = NONE)
+class ChatServiceTest {
+
+    @Autowired
+    private ChatMessageRepository chatMessageRepository;
+
+    @Autowired
+    private ChatService chatService;
+
+    @DisplayName("채팅메세지를 저장할 수 있다.")
+    @Test
+    void saveChatMessage() {
+        // given
+        ChatMessageTextParam chatMessageTextParam = ChatFixture.chatMessageTextParam();
+
+        // when
+        ChatMessageTranslateTextResult result = chatService.translateText(chatMessageTextParam);
+
+        // then
+        assertThat(result.translatedContents()).isEqualTo(chatMessageTextParam.contents());
+    }
+
+}
