3f0d1268b71200f97902ef6617d8c2c9f856f354
byeolhaha <j35635aa@gmail.com>
2024-04-10 18:29:11 +0900

    feat : chatRoom 도메인 구현

---
+++ src/main/java/com/hellomeritz/chat/domain/ChatRoom.java
@@ -0,0 +1,50 @@
+package com.hellomeritz.chat.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.util.Assert;
+
+@Table(name = "chat_rooms")
+@Getter
+@Entity
+public class ChatRoom {
+
+    private static final int USER_ID_MIN_VALUE = 1;
+    private static final int FC_ID_MIN_VALUE = 1;
+
+    protected ChatRoom() {
+    }
+
+    @Column(name = "chat_room_id", nullable = false)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long chatRoomId;
+
+    @Column(name = "fc_id", nullable = false)
+    private long fcId;
+
+    @Column(name = "user_id", nullable = false)
+    private long userId;
+
+    private ChatRoom(
+        long fcId,
+        long userId
+    ) {
+        Assert.isTrue(userId >= USER_ID_MIN_VALUE, "userId는 음수이거나 0일 수 없습니다.");
+        Assert.isTrue(fcId >= FC_ID_MIN_VALUE, "fcId는 음수이거나 0일 수 없습니다.");
+
+        this.fcId = fcId;
+        this.userId = userId;
+    }
+
+    public static ChatRoom of(
+        long fcId,
+        long userId
+    ) {
+        return new ChatRoom(
+            fcId,
+            userId
+        );
+    }
+
+}
