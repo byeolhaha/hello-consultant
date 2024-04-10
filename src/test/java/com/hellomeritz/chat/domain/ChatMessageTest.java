430e0680d8cd52cadd65ab4c5ea03b03d690a08c
byeolhaha <j35635aa@gmail.com>
2024-04-10 14:24:32 +0900

    test : chatMessage 도메인 관련 userId와 roomId 필드 테스트 구현

--- src/test/java/com/hellomeritz/chat/domain/ChatMessageTest.java
+++ src/test/java/com/hellomeritz/chat/domain/ChatMessageTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Stream;
 
@@ -50,6 +51,34 @@ class ChatMessageTest {
                 1,
                 true,
                 1
+            ));
+    }
+
+    @DisplayName("userId가 음수이거나 0인 경우 예외를 던진다.")
+    @ParameterizedTest
+    @ValueSource(longs = {0, -1})
+    void testInvalidUserId(long userId) {
+        assertThrows(IllegalArgumentException.class,
+            () -> new ChatMessage(
+                "안녕하세요",
+                ChatMessageType.TEXT.name(),
+                userId,
+                true,
+                1
+            ));
+    }
+
+    @DisplayName("chatRoomId가 음수이거나 0인 경우 예외를 던진다.")
+    @ParameterizedTest
+    @ValueSource(longs = {0, -1})
+    void testInvalidChatRoomId(long chatRoomId) {
+        assertThrows(IllegalArgumentException.class,
+            () -> new ChatMessage(
+                "안녕하세요",
+                ChatMessageType.TEXT.name(),
+                1,
+                true,
+                chatRoomId
             ));
     }
 
