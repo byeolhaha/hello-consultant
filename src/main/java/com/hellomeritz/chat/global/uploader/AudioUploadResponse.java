1422a9f072ba2e27be8f19183d45feaed26e7a4e
byeolhaha <j35635aa@gmail.com>
2024-04-12 09:20:11 +0900

    feat : stt 서비스 구현

--- src/main/java/com/hellomeritz/chat/global/uploader/AudioUploadResponse.java
+++ src/main/java/com/hellomeritz/chat/global/uploader/AudioUploadResponse.java
@@ -1,10 +1,24 @@
 package com.hellomeritz.chat.global.uploader;
 
+import com.hellomeritz.chat.domain.ChatMessage;
+import com.hellomeritz.chat.domain.ChatMessageType;
+import com.hellomeritz.chat.service.dto.param.ChatMessageSttParam;
+
 public record AudioUploadResponse(
     String audioUrl
 ) {
 
     public static AudioUploadResponse to(String audioUrl) {
         return new AudioUploadResponse(audioUrl);
+    }
+
+    public ChatMessage toChatMessage(ChatMessageSttParam param) {
+        return ChatMessage.of(
+            audioUrl,
+            ChatMessageType.AUDIO.name(),
+            param.userId(),
+            param.isFC(),
+            param.chatRoomId()
+        );
     }
 }
