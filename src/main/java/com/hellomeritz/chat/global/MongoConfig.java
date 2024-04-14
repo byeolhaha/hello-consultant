308ff065c802b9165a72dd7e571e148ce85c0ea6
byeolhaha <j35635aa@gmail.com>
2024-04-14 23:58:47 +0900

    refactor : mongodb 관련 설정 ec2 연결

--- src/main/java/com/hellomeritz/chat/global/MongoConfig.java
+++ src/main/java/com/hellomeritz/chat/global/MongoConfig.java
@@ -2,6 +2,7 @@ package com.hellomeritz.chat.global;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoOperations;
@@ -10,8 +11,11 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 @Configuration
 public class MongoConfig {
 
-    private static final String MONGODB_URL = "mongodb://localhost:27017";
-    private static final String DATABASE_NAME = "meritz";
+    @Value("${mongodb.url}")
+    private String MONGODB_URL;
+
+    @Value("${mongodb.database}")
+    private String DATABASE_NAME;
 
     @Bean
     MongoClient mongoClient() {
