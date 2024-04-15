400dad3c0db88606d96bb93dcc34c1578d17a17c
user <j35635aa@gmail.com>
2024-04-15 10:42:27 +0900

    style : test 관련 설정 변경

--- src/test/java/com/hellomeritz/HellomeritzApplicationTests.java
+++ src/test/java/com/hellomeritz/HellomeritzApplicationTests.java
@@ -3,7 +3,7 @@ package com.hellomeritz;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = HellomeritzApplicationTests.class)
 class HellomeritzApplicationTests {
 
 	@Test
