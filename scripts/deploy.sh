18112954c08c90803b6e2cca5bd27b2f9e7dedd4
user <j35635aa@gmail.com>
2024-04-15 16:22:35 +0900

    chore : deploy script 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,18 +4,18 @@
 JAR_PATH="/home/ubuntu/meritz/build/libs/hellomeritz-0.0.1-SNAPSHOT.jar"
 
 # 실행 중인 애플리케이션의 PID 찾기
-CURRENT_PID=$(pgrep -f $JAR_PATH)
+CURRENT_PID=$(pgrep -f "java -jar $JAR_PATH")
 
 # 실행 중인 애플리케이션이 있으면 종료
-if [[ -n $CURRENT_PID ]]; then
+if [ -n "$CURRENT_PID" ]; then
   echo "종료할 프로세스 PID: $CURRENT_PID"
-  sudo kill $CURRENT_PID
+  sudo kill "$CURRENT_PID"
 
   # 프로세스가 종료될 때까지 대기
-  while ps -p $CURRENT_PID &>/dev/null; do
+  while ps -p "$CURRENT_PID" &>/dev/null; do
     sleep 1
   done
 fi
 
 # 애플리케이션 실행
-nohup java -jar $JAR_PATH &>/dev/null &
+nohup java -jar "$JAR_PATH" &>/dev/null &
