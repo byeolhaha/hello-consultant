294e98c19b6ed38fb4788306d9d42adf286b202e
user <j35635aa@gmail.com>
2024-04-16 12:42:20 +0900

    refactor : credentials 파일 직접 읽도록 수정

--- src/main/java/com/hellomeritz/chat/global/stt/SttGoogleManager.java
+++ src/main/java/com/hellomeritz/chat/global/stt/SttGoogleManager.java
@@ -4,13 +4,18 @@ import com.google.api.gax.longrunning.OperationFuture;
 import com.google.api.gax.longrunning.OperationTimedPollAlgorithm;
 import com.google.api.gax.retrying.RetrySettings;
 import com.google.api.gax.retrying.TimedRetryAlgorithm;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.speech.v1.SpeechSettings;
+import com.google.api.gax.core.FixedCredentialsProvider;
 import com.google.cloud.speech.v1.*;
 import com.hellomeritz.chat.global.exception.ErrorCode;
 import com.hellomeritz.chat.global.exception.custom.SttException;
-import com.hellomeritz.chat.global.uploader.AudioUploadResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.threeten.bp.Duration;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -18,26 +23,28 @@ import java.util.concurrent.ExecutionException;
 @Component
 public class SttGoogleManager implements SttManager {
 
-    public SttResponse asyncRecognizeGcs(SttRequest request) {
-        String gcsUri = request.audioUrl().replace("https://storage.googleapis.com/", "gs://");
+    private static final String GOOGLE_BUCKET_PATH = "https://storage.googleapis.com/";
+    private static final String GOOGLE_BUCKET_HOST = "gs://";
 
-        // Configure polling algorithm
-        SpeechSettings.Builder speechSettings = SpeechSettings.newBuilder();
-        TimedRetryAlgorithm timedRetryAlgorithm =
-                OperationTimedPollAlgorithm.create(
-                        RetrySettings.newBuilder()
-                                .setInitialRetryDelay(Duration.ofMillis(500L))
-                                .setRetryDelayMultiplier(1.5)
-                                .setMaxRetryDelay(Duration.ofMillis(5000L))
-                                .setInitialRpcTimeout(Duration.ZERO) // ignored
-                                .setRpcTimeoutMultiplier(1.0) // ignored
-                                .setMaxRpcTimeout(Duration.ZERO) // ignored
-                                .setTotalTimeout(Duration.ofHours(24L)) // set polling timeout to 24 hours
-                                .build());
-        speechSettings.longRunningRecognizeOperationSettings().setPollingAlgorithm(timedRetryAlgorithm);
+    @Value("${google.stt.credential.path}")
+    private String credentialsPath;
+
+    public SttResponse asyncRecognizeGcs(SttRequest request) {
+        String gcsUri = request.audioUrl().replace(GOOGLE_BUCKET_PATH, GOOGLE_BUCKET_HOST);
+
+        SpeechSettings.Builder speechSettingsBuilder = getCredentials();
+        TimedRetryAlgorithm timedRetryAlgorithm = setTimeRetry();
+        speechSettingsBuilder.longRunningRecognizeOperationSettings().setPollingAlgorithm(timedRetryAlgorithm);
+
+        SpeechSettings speechSettings = null;
+        try {
+            speechSettings = speechSettingsBuilder.build();
+        } catch (IOException e) {
+            throw new SttException(ErrorCode.STT_SPEECH_ERROR);
+        }
 
         // Instantiates a client with GOOGLE_APPLICATION_CREDENTIALS
-        try (SpeechClient speech = SpeechClient.create(speechSettings.build())) {
+        try (SpeechClient speech = SpeechClient.create(speechSettings)) {
 
             // Configure remote file request for FLAC
             RecognitionConfig config =
@@ -70,6 +77,30 @@ public class SttGoogleManager implements SttManager {
         }
 
         return SttResponse.emptySttResponse();
+    }
+
+    private SpeechSettings.Builder getCredentials() {
+        try {
+            GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(credentialsPath));
+            return SpeechSettings.newBuilder()
+                    .setCredentialsProvider(FixedCredentialsProvider.create(credentials))
+                    .build().toBuilder();
+        } catch (IOException e) {
+            throw new SttException(ErrorCode.STT_CREDENTIAL_ERROR);
+        }
+    }
+
+    private TimedRetryAlgorithm setTimeRetry() {
+        return OperationTimedPollAlgorithm.create(
+                RetrySettings.newBuilder()
+                        .setInitialRetryDelay(Duration.ofMillis(500L))
+                        .setRetryDelayMultiplier(1.5)
+                        .setMaxRetryDelay(Duration.ofMillis(5000L))
+                        .setInitialRpcTimeout(Duration.ZERO)
+                        .setRpcTimeoutMultiplier(1.0)
+                        .setMaxRpcTimeout(Duration.ZERO)
+                        .setTotalTimeout(Duration.ofHours(24L)) // set polling timeout to 24 hours
+                        .build());
     }
 
 }
