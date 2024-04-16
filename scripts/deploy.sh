7578eaa18cfa2af6bb8cc04a09ac107a761b196d
user <j35635aa@gmail.com>
2024-04-16 09:23:19 +0900

    chore : log 파일 생성을 위한 코드 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,8 @@
 # JAR 파일 경로 설정
 JAR_PATH="/home/ubuntu/meritz/build/libs/hellomeritz-0.0.1-SNAPSHOT.jar"
 
+LOG_FILE="/var/log/hellomeritz.log"
+
 # 실행 중인 애플리케이션의 PID 찾기
 CURRENT_PID=$(pgrep -f "java -jar $JAR_PATH")
 
@@ -18,4 +20,4 @@ if [ -n "$CURRENT_PID" ]; then
 fi
 
 # 애플리케이션 실행
-nohup java -jar "$JAR_PATH" &>/dev/null &
+nohup java -jar "$JAR_PATH" >> "$LOG_FILE" 2>&1 &
