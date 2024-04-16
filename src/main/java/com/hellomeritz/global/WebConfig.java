f72574776440512264d369d6fbfc4f57c031fa6e
user <j35635aa@gmail.com>
2024-04-16 09:03:49 +0900

    feat : cors 에러 해결

---
+++ src/main/java/com/hellomeritz/global/WebConfig.java
@@ -0,0 +1,15 @@
+package com.hellomeritz.global;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .maxAge(3600);
+    }
+}
