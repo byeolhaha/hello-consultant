acf4795afeca60de78e3b6d66982495f90407ebf
user <j35635aa@gmail.com>
2024-04-16 14:40:47 +0900

    feat : 각 open api별 language에 대한 enum 구현

---
+++ src/main/java/com/hellomeritz/chat/global/TargetLanguage.java
@@ -0,0 +1,30 @@
+package com.hellomeritz.chat.global;
+
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@Getter
+public enum TargetLanguage {
+    중국("ZH"),
+    미국("EN-US"),
+    영국("EN-GB"),
+    러시아("RU"),
+    일본("JA"),
+    한국("KO"),
+    프랑스("FR"),
+    스페인("ES"),
+    이탈리아("IT");
+
+    private String deeplLang;
+
+    TargetLanguage(String deeplLang) {
+        this.deeplLang = deeplLang;
+    }
+
+    public static boolean checkTranslatorFormat(String targetLang) {
+        return Arrays.stream(TargetLanguage.values())
+                .noneMatch(targetLanguage -> targetLanguage.deeplLang.equals(targetLang));
+    }
+
+}
