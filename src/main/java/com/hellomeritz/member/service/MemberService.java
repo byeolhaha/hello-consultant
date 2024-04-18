eb680c805f67e9d2d8a4e5fd6d911477493911bf
user <j35635aa@gmail.com>
2024-04-18 16:46:17 +0900

    feat : 외국인 ip 주소 저장 서비스 구현

--- src/main/java/com/hellomeritz/member/service/MemberService.java
+++ src/main/java/com/hellomeritz/member/service/MemberService.java
@@ -1,10 +1,13 @@
 package com.hellomeritz.member.service;
 
 import com.hellomeritz.member.domain.Foreigner;
+import com.hellomeritz.member.global.IpSensor;
 import com.hellomeritz.member.repository.ForeignRepository;
 import com.hellomeritz.member.service.dto.param.ForeignInfoSaveParam;
+import com.hellomeritz.member.service.dto.param.ForeignSaveIpAddressParam;
 import com.hellomeritz.member.service.dto.result.ForeignCreateResult;
 import com.hellomeritz.member.service.dto.result.ForeignInfoSaveResult;
+import com.hellomeritz.member.service.dto.result.ForeignSaveIpAddressResult;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,9 +15,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final ForeignRepository foreignRepository;
+    private final IpSensor ipSensor;
 
-    public MemberService(ForeignRepository foreignRepository) {
+    public MemberService(ForeignRepository foreignRepository, IpSensor ipSensor) {
         this.foreignRepository = foreignRepository;
+        this.ipSensor = ipSensor;
     }
 
     @Transactional
@@ -29,6 +34,16 @@ public class MemberService {
         Foreigner foreigner = foreignRepository.save(Foreigner.of());
 
         return ForeignCreateResult.of(foreigner.getId());
+    }
+
+    @Transactional
+    public ForeignSaveIpAddressResult saveForeignIpAddress(ForeignSaveIpAddressParam param) {
+        Foreigner foreigner = foreignRepository.getById(param.userId());
+        String clientIp = ipSensor.getClientIP();
+
+        foreigner.updateIpAddress(clientIp);
+
+        return ForeignSaveIpAddressResult.to(clientIp);
     }
 
 }
