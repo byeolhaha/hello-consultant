6676268445be16baf4dc9c1121dffcb3d4fa633d
user <j35635aa@gmail.com>
2024-04-18 18:07:42 +0900

    feat : 설계사인지 여부 조회하는 쿼리 구현

--- src/main/java/com/hellomeritz/member/repository/ForeignJpaRepository.java
+++ src/main/java/com/hellomeritz/member/repository/ForeignJpaRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.repository.query.Param;
 
 public interface ForeignJpaRepository extends JpaRepository<Foreigner, Long> {
 
-    @Query("select coalesce(f.id, 0) from Foreigner f where f.ipAddress = :ipAddress")
-    Long getUserId(@Param(value = "ipAddress") String ipAddress);
+    @Query("select case when exists(select 1 from Foreigner f where f.id = :userId and f.ipAddress = :ipAddress) then true else false end from Foreigner f")
+    boolean isFc(@Param(value = "ipAddress") String ipAddress, @Param(value = "userId") Long userId);
 }
--- src/main/java/com/hellomeritz/member/repository/ForeignRepository.java
+++ src/main/java/com/hellomeritz/member/repository/ForeignRepository.java
@@ -1,12 +1,13 @@
 package com.hellomeritz.member.repository;
 
 import com.hellomeritz.member.domain.Foreigner;
+import org.springframework.data.repository.query.Param;
 
 public interface ForeignRepository {
 
     Foreigner save(Foreigner foreigner);
 
-    Long getUserId(String macAddress);
+    boolean isFc(String ipAddress, Long userId);
 
     Foreigner getById(Long userId);
 }
