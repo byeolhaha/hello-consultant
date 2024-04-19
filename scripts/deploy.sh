8c862fd1713f891b2e0cd43d3042f5f481e3164e
user <j35635aa@gmail.com>
2024-04-19 14:31:03 +0900

    chore : 로그 파일 설정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,6 +18,5 @@ if [ -n "$CURRENT_PID" ]; then
 fi
 
 # 애플리케이션 실행
-nohup java -jar "$JAR_PATH" &>/dev/null &
-
+#nohup java -jar "$JAR_PATH" &>/dev/null &
 nohup java -jar "$JAR_PATH" --logging.file.path=/home/ec2-user/ --logging.level.org.hibernate.SQL=DEBUG >> /home/ec2-user/deploy.log 2>/home/ec2-user/deploy_err.log &
