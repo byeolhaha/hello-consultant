b997f1738bfffa2a81faeff72977e0ae2706e144
byeolhaha <j35635aa@gmail.com>
2024-04-20 20:07:26 +0900

    feat : 설계사 레파지토리 구현

---
+++ src/main/java/com/hellomeritz/member/repository/fc/FinancialConsultantJapRepository.java
@@ -0,0 +1,8 @@
+package com.hellomeritz.member.repository.fc;
+
+import com.hellomeritz.member.domain.FinancialConsultant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FinancialConsultantJapRepository extends JpaRepository<FinancialConsultant, Long> {
+
+}
