b6fea680979f8bfc9966deefae73b2e3b9b42c2f
byeolhaha <j35635aa@gmail.com>
2024-04-23 13:07:16 +0900

    style : 불필요한 import 삭제

--- src/main/java/com/hellomeritz/chat/global/uploader/AudioUploader.java
+++ src/main/java/com/hellomeritz/chat/global/uploader/AudioUploader.java
@@ -2,8 +2,6 @@ package com.hellomeritz.chat.global.uploader;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
 public interface AudioUploader {
 
     AudioUploadResponse upload(MultipartFile multipartFile);
