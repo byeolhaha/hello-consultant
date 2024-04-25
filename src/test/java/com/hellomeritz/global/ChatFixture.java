38a70d347ce4596f6da6c7bdeacfef46bbcb36fc
user <j35635aa@gmail.com>
2024-04-25 19:36:54 +0900

    test : 패스워드 생성 및 조회 관련 rest docs test

--- src/test/java/com/hellomeritz/global/ChatFixture.java
+++ src/test/java/com/hellomeritz/global/ChatFixture.java
@@ -1,8 +1,6 @@
 package com.hellomeritz.global;
 
-import com.hellomeritz.chat.controller.dto.request.ChatMessageSttRequest;
-import com.hellomeritz.chat.controller.dto.request.ChatMessageTranslateRequest;
-import com.hellomeritz.chat.controller.dto.request.ChatRoomCreateRequest;
+import com.hellomeritz.chat.controller.dto.request.*;
 import com.hellomeritz.chat.controller.dto.response.ChatRoomUserInfoResponse;
 import com.hellomeritz.chat.domain.ChatMessage;
 import com.hellomeritz.chat.domain.ChatMessageType;
@@ -29,112 +27,112 @@ public class ChatFixture {
 
     public static ChatMessageTextParam chatMessageTextParamByFC() {
         return new ChatMessageTextParam(
-            "안녕하세요 반가워요",
-            FC_ID,
-            true,
-            1,
-            TargetLanguage.findTargetLanguage("US"),
-            SourceLanguage.findSourceLanguage("KOREAN")
+                "안녕하세요 반가워요",
+                FC_ID,
+                true,
+                1,
+                TargetLanguage.findTargetLanguage("US"),
+                SourceLanguage.findSourceLanguage("KOREAN")
         );
     }
 
     public static ChatMessage originChatMessageByFC() {
         return ChatMessage.of(
-            "안녕하세요 반가워요",
-            ChatMessageType.TEXT.name(),
-            FC_ID,
-            true,
-            1
+                "안녕하세요 반가워요",
+                ChatMessageType.TEXT.name(),
+                FC_ID,
+                true,
+                1
         );
     }
 
     public static ChatMessage translatedChatMessageByFC() {
         return ChatMessage.of(
-            "hello nice meet you",
-            ChatMessageType.TRANSLATED_TEXT.name(),
-            FC_ID,
-            true,
-            1
+                "hello nice meet you",
+                ChatMessageType.TRANSLATED_TEXT.name(),
+                FC_ID,
+                true,
+                1
         );
     }
 
     public static ChatMessage originChatMessageByUser() {
         return ChatMessage.of(
-            "Hello my name is byeol",
-            ChatMessageType.TEXT.name(),
-            FC_ID,
-            true,
-            1
+                "Hello my name is byeol",
+                ChatMessageType.TEXT.name(),
+                FC_ID,
+                true,
+                1
         );
     }
 
     public static ChatMessage translatedChatMessageByUser() {
         return ChatMessage.of(
-            "안녕하세요 저의 이름은 별입니다.",
-            ChatMessageType.TRANSLATED_TEXT.name(),
-            FC_ID,
-            true,
-            1
+                "안녕하세요 저의 이름은 별입니다.",
+                ChatMessageType.TRANSLATED_TEXT.name(),
+                FC_ID,
+                true,
+                1
         );
     }
 
     public static ChatMessageTextParam chatMessageTextParamByUser() {
         return new ChatMessageTextParam(
-            "Hello my name is byeol",
-            USER_ID,
-            false,
-            1,
-            TargetLanguage.findTargetLanguage("US"),
-            SourceLanguage.findSourceLanguage("KOREAN")
+                "Hello my name is byeol",
+                USER_ID,
+                false,
+                1,
+                TargetLanguage.findTargetLanguage("US"),
+                SourceLanguage.findSourceLanguage("KOREAN")
         );
     }
 
     public static ChatRoomCreateParam chatRoomCreateParam() {
         return new ChatRoomCreateParam(
-            1L,
-            1L
+                1L,
+                1L
         );
     }
 
     public static ChatMessageGetParam chatMessageGetParam() {
         return new ChatMessageGetParam(
-            FC_ID,
-            CHAT_MESSAGE_ID_MIN_VALUE,
-            1,
-            true
+                FC_ID,
+                CHAT_MESSAGE_ID_MIN_VALUE,
+                1,
+                true
         );
     }
 
     public static ChatMessageGetResults chatMessageGetResults() {
         return new ChatMessageGetResults(
-            List.of(
-                new ChatMessageGetResults.ChatMessageGetResult(
-                    "66172f7ab156dc2cf99c4b2c",
-                    "안녕하세요 반갑습니다.",
-                    LocalDateTime.of(2024, 3, 1, 11, 20).toString(),
-                    true
+                List.of(
+                        new ChatMessageGetResults.ChatMessageGetResult(
+                                "66172f7ab156dc2cf99c4b2c",
+                                "안녕하세요 반갑습니다.",
+                                LocalDateTime.of(2024, 3, 1, 11, 20).toString(),
+                                true
+                        ),
+                        new ChatMessageGetResults.ChatMessageGetResult(
+                                "66172f7ab156dc2cf99c4b2d",
+                                "hello nice meet you",
+                                LocalDateTime.of(2024, 3, 1, 11, 21).toString(),
+                                true
+                        ),
+                        new ChatMessageGetResults.ChatMessageGetResult(
+                                "66172f7ab156dc2cf99c4b2e",
+                                "hello my name is byeol",
+                                LocalDateTime.of(2024, 3, 1, 11, 22).toString(),
+                                false
+                        ),
+                        new ChatMessageGetResults.ChatMessageGetResult(
+                                "66172f7ab156dc2cf99c4b2f",
+                                "안녕하세요 제 이름은 별입니다.",
+                                LocalDateTime.of(2024, 3, 1, 11, 23).toString(),
+                                true
+                        )
                 ),
-                new ChatMessageGetResults.ChatMessageGetResult(
-                    "66172f7ab156dc2cf99c4b2d",
-                    "hello nice meet you",
-                    LocalDateTime.of(2024, 3, 1, 11, 21).toString(),
-                    true
-                ),
-                new ChatMessageGetResults.ChatMessageGetResult(
-                    "66172f7ab156dc2cf99c4b2e",
-                    "hello my name is byeol",
-                    LocalDateTime.of(2024, 3, 1, 11, 22).toString(),
-                    false
-                ),
-                new ChatMessageGetResults.ChatMessageGetResult(
-                    "66172f7ab156dc2cf99c4b2f",
-                    "안녕하세요 제 이름은 별입니다.",
-                    LocalDateTime.of(2024, 3, 1, 11, 23).toString(),
-                    true
-                )
-            ),
-            "66172f7ab156dc2cf99c4b2c",
-            true
+                "66172f7ab156dc2cf99c4b2c",
+                true
         );
     }
 
@@ -153,49 +151,49 @@ public class ChatFixture {
 
     public static ChatMessageSttResult chatMessageSttResult() {
         return new ChatMessageSttResult(
-            "Hello my name is byeol",
-            LocalDateTime.now().toString(),
-            SttProvider.WHISPER.name()
+                "Hello my name is byeol",
+                LocalDateTime.now().toString(),
+                SttProvider.WHISPER.name()
         );
     }
 
 
     public static ChatMessageTranslateResult chatMessageTranslateResult() {
         return new ChatMessageTranslateResult(
-            "hello my name is kim byeol",
-            "안녕하세요 내 이름은 김별입니다",
-            LocalDateTime.now()
+                "hello my name is kim byeol",
+                "안녕하세요 내 이름은 김별입니다",
+                LocalDateTime.now()
         );
     }
 
     public static ChatMessageTranslateRequest chatMessageTranslateRequest() {
         return new ChatMessageTranslateRequest(
-            "hello my name is kim byeol",
-            1L,
-            true,
-            "KOREAN",
-            "US"
+                "hello my name is kim byeol",
+                1L,
+                true,
+                "KOREAN",
+                "US"
         );
     }
 
     public static ChatRoom chatRoom() {
         return ChatRoom.of(
-            1L,
-            1L
+                1L,
+                1L
         );
     }
 
     public static ChatRoomUserInfoResponse chatRoomUserInfoResponse() {
         return new ChatRoomUserInfoResponse(
-            1L,
-            1L
+                1L,
+                1L
         );
     }
 
     public static ChatRoomUserInfoResult chatRoomUserInfoResult() {
         return new ChatRoomUserInfoResult(
-            1L,
-            1L
+                1L,
+                1L
         );
     }
 
@@ -214,6 +212,20 @@ public class ChatFixture {
     ) {
         return new ChatRoomPasswordCheckParam(
                 chatRoomId,
+                "30303TWSA!!!"
+        );
+    }
+
+    public static ChatRoomPasswordCreateRequest chatRoomPasswordCreateRequest(
+    ) {
+        return new ChatRoomPasswordCreateRequest(
+                "30303TWSA!!!"
+        );
+    }
+
+    public static ChatRoomPasswordCheckRequest chatRoomPasswordCheckRequest(
+    ) {
+        return new ChatRoomPasswordCheckRequest(
                 "30303TWSA!!!"
         );
     }
