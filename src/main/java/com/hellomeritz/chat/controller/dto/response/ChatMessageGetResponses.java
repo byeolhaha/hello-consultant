bad4214017b17b7abd5b5be15013f6f028da6e5d
byeolhaha <j35635aa@gmail.com>
2024-05-09 17:12:28 +0900

    style : 메세지 목록에 읽음 여부 필드 추가

--- src/main/java/com/hellomeritz/chat/controller/dto/response/ChatMessageGetResponses.java
+++ src/main/java/com/hellomeritz/chat/controller/dto/response/ChatMessageGetResponses.java
@@ -5,30 +5,32 @@ import com.hellomeritz.chat.service.dto.result.ChatMessageGetResults;
 import java.util.List;
 
 public record ChatMessageGetResponses(
-        List<ChatMessageGetResponse> chatMessages,
-        String nextChatMessageId,
-        boolean hasNext
+    List<ChatMessageGetResponse> chatMessages,
+    String nextChatMessageId,
+    boolean hasNext
 
 ) {
     public static ChatMessageGetResponses to(ChatMessageGetResults results) {
         return new ChatMessageGetResponses(
-                results.chatMessages().stream()
-                        .map(chatMessageGetResult -> new ChatMessageGetResponse(
-                                chatMessageGetResult.chatMessageId(),
-                                chatMessageGetResult.contents(),
-                                chatMessageGetResult.createdAt(),
-                                chatMessageGetResult.isFC()
-                        )).toList(),
-                results.nextChatMessageId(),
-                results.hasNext()
+            results.chatMessages().stream()
+                .map(chatMessageGetResult -> new ChatMessageGetResponse(
+                    chatMessageGetResult.chatMessageId(),
+                    chatMessageGetResult.contents(),
+                    chatMessageGetResult.createdAt(),
+                    chatMessageGetResult.isFC(),
+                    chatMessageGetResult.readOrNot()
+                )).toList(),
+            results.nextChatMessageId(),
+            results.hasNext()
         );
     }
 
     public record ChatMessageGetResponse(
-            String chatMessageId,
-            String contents,
-            String createdAt,
-            boolean isFC
+        String chatMessageId,
+        String contents,
+        String createdAt,
+        boolean isFC,
+        boolean readOrNot
     ) {
 
     }
--- src/main/java/com/hellomeritz/chat/service/dto/result/ChatMessageGetResults.java
+++ src/main/java/com/hellomeritz/chat/service/dto/result/ChatMessageGetResults.java
@@ -8,31 +8,33 @@ import java.util.Comparator;
 import java.util.List;
 
 public record ChatMessageGetResults(
-        List<ChatMessageGetResult> chatMessages,
-        String nextChatMessageId,
-        boolean hasNext
+    List<ChatMessageGetResult> chatMessages,
+    String nextChatMessageId,
+    boolean hasNext
 ) {
 
     public static ChatMessageGetResults to(
-            ChatMessageGetRepositoryResponses chatMessages) {
+        ChatMessageGetRepositoryResponses chatMessages) {
         return new ChatMessageGetResults(
-                chatMessages.chatMessages().stream()
-                        .map(chatMessage -> new ChatMessageGetResult(
-                                chatMessage.getId(),
-                                chatMessage.getContents(),
-                                chatMessage.getCreatedAt().toString(),
-                                chatMessage.isFC()
-                        )).toList(),
-                chatMessages.nextChatMessageId(),
-                chatMessages.hasNext()
+            chatMessages.chatMessages().stream()
+                .map(chatMessage -> new ChatMessageGetResult(
+                    chatMessage.getId(),
+                    chatMessage.getContents(),
+                    chatMessage.getCreatedAt().toString(),
+                    chatMessage.getIsFC(),
+                    chatMessage.getReadOrNot()
+                )).toList(),
+            chatMessages.nextChatMessageId(),
+            chatMessages.hasNext()
         );
     }
 
     public record ChatMessageGetResult(
-            String chatMessageId,
-            String contents,
-            String createdAt,
-            boolean isFC
+        String chatMessageId,
+        String contents,
+        String createdAt,
+        boolean isFC,
+        boolean readOrNot
     ) {
 
     }
