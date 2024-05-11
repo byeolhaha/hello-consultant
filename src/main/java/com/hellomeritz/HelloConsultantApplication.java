3962c5a1db57031ba6ece20e4d8bb28809091483
byeolhaha <j35635aa@gmail.com>
2024-05-11 21:07:05 +0900

    style : 이름 변경 및 파일 위치 이동에 따른 수정

--- src/main/java/com/hellomeritz/HelloConsultantApplication.java
+++ src/main/java/com/hellomeritz/HelloConsultantApplication.java
@@ -1,16 +1,14 @@
 package com.hellomeritz;
 
-import com.hellomeritz.chat.domain.BanWord;
-import com.hellomeritz.chat.service.BanWordService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
-public class HellomeritzApplication {
+public class HelloConsultantApplication {
     public static void main(String[] args) {
-		SpringApplication.run(HellomeritzApplication.class, args);
+		SpringApplication.run(HelloConsultantApplication.class, args);
 	}
 
 }
