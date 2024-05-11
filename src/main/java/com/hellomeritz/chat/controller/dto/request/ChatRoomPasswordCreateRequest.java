ce9f0d75cfcd16f6c31db6cef6c712c230c88f08
byeolhaha <j35635aa@gmail.com>
2024-05-11 21:08:53 +0900

    refactor : 필드 추가 및 그에 대한 validation 추가

--- src/main/java/com/hellomeritz/chat/controller/dto/request/ChatRoomPasswordCreateRequest.java
+++ src/main/java/com/hellomeritz/chat/controller/dto/request/ChatRoomPasswordCreateRequest.java
@@ -4,16 +4,21 @@ import com.hellomeritz.chat.domain.ChatRoomPassword;
 import com.hellomeritz.chat.service.dto.param.ChatRoomCreateParam;
 import com.hellomeritz.chat.service.dto.param.ChatRoomPasswordCreateParam;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.time.LocalDateTime;
 
 public record ChatRoomPasswordCreateRequest(
-
         @NotBlank(message = "chatRoomPassword은 빈값일 수 없습니다.")
-        String chatRoomPassword
+        String chatRoomPassword,
+        @NotNull(message = "chatRoomId는 null 값일 수 없습니다.")
+        @Positive(message = "chatRoomId는 양수여야 합니다.")
+        Long chatRoomId
 ) {
 
-    public ChatRoomPasswordCreateParam toChatRoomPasswordCreateParam(long chatRoomId) {
+    public ChatRoomPasswordCreateParam toChatRoomPasswordCreateParam() {
         return new ChatRoomPasswordCreateParam(
                 ChatRoomPassword.of(chatRoomPassword),
                 chatRoomId,
