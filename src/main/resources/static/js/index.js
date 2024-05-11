d07b51395f9294d3030592cda8d3ec5afadfe607
byeolhaha <j35635aa@gmail.com>
2024-05-11 11:02:14 +0900

    feat : 채팅 시작 버튼 클릭시 새로운 채팅, 기존 채팅 선택 모달창 등장하도록 기능 추가

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,21 @@
+document.getElementById("openModal").addEventListener("click", function() {
+    document.getElementById("myModal").style.display = "block";
+});
+
+document.getElementById("newCustomer").addEventListener("click", function() {
+    window.location.href = "/survey.html";
+});
+
+document.getElementById("existingCustomer").addEventListener("click", function() {
+    window.location.href = "/client-chatrooms.html";
+});
+
+document.getElementsByClassName("close")[0].addEventListener("click", function() {
+    document.getElementById("myModal").style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+    if (event.target == document.getElementById("myModal")) {
+        document.getElementById("myModal").style.display = "none";
+    }
+});
