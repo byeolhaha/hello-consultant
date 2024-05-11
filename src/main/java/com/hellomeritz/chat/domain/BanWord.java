7c084b0bac37b83733bae00c604043c6cc179ab8
byeolhaha <j35635aa@gmail.com>
2024-05-11 20:50:34 +0900

    feat : 금칙어 도메인 구현

---
+++ src/main/java/com/hellomeritz/chat/domain/BanWord.java
@@ -0,0 +1,45 @@
+package com.hellomeritz.chat.domain;
+
+import com.hellomeritz.chat.global.SourceLanguage;
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.util.Assert;
+
+@Getter
+@Entity
+public class BanWord {
+
+    @Column(name = "ban_word_id", nullable = false)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long banWordId;
+
+    @Column(name = "text", nullable = false)
+    private String text;
+
+    @Column(name = "source_language", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private SourceLanguage sourceLanguage;
+
+    protected BanWord() {}
+
+    private BanWord(
+        String text,
+        SourceLanguage sourceLanguage
+    ) {
+        Assert.hasLength(text, "text는 필수값입니다.");
+        this.text = text;
+        this.sourceLanguage = sourceLanguage;
+    }
+
+    public static BanWord to(
+        String text,
+        SourceLanguage sourceLanguage
+    ) {
+        return new BanWord(
+            text,
+            sourceLanguage
+        );
+    }
+
+}
