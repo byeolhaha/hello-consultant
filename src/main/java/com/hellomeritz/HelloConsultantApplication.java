eafa19c43ce722b81400f7d653d10c237b536580
byeolhaha <j35635aa@gmail.com>
2024-05-11 20:47:36 +0900

    refactor : resource 관련 설정

--- src/main/java/com/hellomeritz/HelloConsultantApplication.java
+++ src/main/java/com/hellomeritz/HelloConsultantApplication.java
@@ -1,5 +1,7 @@
 package com.hellomeritz;
 
+import com.hellomeritz.chat.domain.BanWord;
+import com.hellomeritz.chat.service.BanWordService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -7,8 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @SpringBootApplication
 public class HellomeritzApplication {
-
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		SpringApplication.run(HellomeritzApplication.class, args);
 	}
 
--- src/main/java/com/hellomeritz/global/WebConfig.java
+++ src/main/java/com/hellomeritz/global/WebConfig.java
@@ -3,9 +3,13 @@ package com.hellomeritz.global;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hellomeritz.member.global.IpSensor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -28,6 +32,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry){
         registry.addInterceptor(new IpSensor(objectMapper))
                 .addPathPatterns("/users/*");
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+            .addResourceLocations("classpath:/templates/","classpath:/static/")
+            .setCacheControl(CacheControl.maxAge(1, TimeUnit.SECONDS));
     }
 
 
