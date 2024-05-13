045f6959242bf28c708fe7df4e80e5fb6761330c
byeolhaha <j35635aa@gmail.com>
2024-05-13 12:01:37 +0900

    refactor : 비밀번호 및 salt 필드 추가에 따른 수정

--- src/main/java/com/hellomeritz/member/domain/FinancialConsultant.java
+++ src/main/java/com/hellomeritz/member/domain/FinancialConsultant.java
@@ -1,7 +1,9 @@
 package com.hellomeritz.member.domain;
 
+import com.hellomeritz.global.encryption.PassWord;
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.springframework.util.Assert;
 
 @Table(name = "financial_consultant")
 @Getter
@@ -28,6 +30,15 @@ public class FinancialConsultant {
     @Column(name = "consultation_state")
     private String consultationState;
 
+    @Column(name = "consultant_password")
+    private String consultantPassword;
+
+    @Column(name = "salt")
+    private String salt;
+
+    @Column(name = "employee_number")
+    private String employeeNumber;
+
     protected FinancialConsultant() {
     }
 
@@ -37,6 +48,11 @@ public class FinancialConsultant {
         String profileUrl,
         String introduceMessage
     ) {
+        Assert.hasLength(phoneNumber, "phoneNumber는 null이거나 빈값일 수 없습니다.");
+        Assert.hasLength(name, "name는 null이거나 빈값일 수 없습니다.");
+        Assert.hasLength(profileUrl, "profileUrl는 null이거나 빈값일 수 없습니다.");
+        Assert.hasLength(introduceMessage, "introduceMessage는 null이거나 빈값일 수 없습니다.");
+
         this.phoneNumber = phoneNumber;
         this.name = name;
         this.profileUrl = profileUrl;
@@ -58,6 +74,53 @@ public class FinancialConsultant {
         );
     }
 
+    private FinancialConsultant(
+        String employeeNumber,
+        String consultantPassword,
+        String salt,
+        String phoneNumber,
+        String name,
+        String profileUrl,
+        String introduceMessage
+    ) {
+        Assert.hasLength(employeeNumber, "employeeNumber는 null이거나 빈값일 수 없습니다.");
+        Assert.hasLength(consultantPassword, "consultantPassword는 null이거나 빈값일 수 없습니다.");
+        Assert.hasLength(salt, "salt는 null이거나 빈값일 수 없습니다.");
+        Assert.hasLength(phoneNumber, "phoneNumber는 null이거나 빈값일 수 없습니다.");
+        Assert.hasLength(name, "name는 null이거나 빈값일 수 없습니다.");
+        Assert.hasLength(profileUrl, "profileUrl는 null이거나 빈값일 수 없습니다.");
+        Assert.hasLength(introduceMessage, "introduceMessage는 null이거나 빈값일 수 없습니다.");
+
+        this.employeeNumber = employeeNumber;
+        this.consultantPassword = consultantPassword;
+        this.salt = salt;
+        this.phoneNumber = phoneNumber;
+        this.name = name;
+        this.profileUrl = profileUrl;
+        this.introduceMessage = introduceMessage;
+        this.consultationState = ConsultationState.AVAILABLE.name();
+    }
+
+    public static FinancialConsultant of(
+        String employeeNumber,
+        String consultantPassword,
+        String salt,
+        String phoneNumber,
+        String name,
+        String profileUrl,
+        String introduceMessage
+    ) {
+        return new FinancialConsultant(
+            employeeNumber,
+            consultantPassword,
+            salt,
+            phoneNumber,
+            name,
+            profileUrl,
+            introduceMessage
+        );
+    }
+
     public void startConsulting() {
         this.consultationState = ConsultationState.UNAVAILABLE.name();
     }
@@ -66,4 +129,11 @@ public class FinancialConsultant {
         this.consultationState = ConsultationState.AVAILABLE.name();
     }
 
+    public void changePassword(String consultantPassword) {
+        this.consultantPassword = consultantPassword;
+    }
+
+    public void changeSalt(String salt) {
+        this.salt = salt;
+    }
 }
