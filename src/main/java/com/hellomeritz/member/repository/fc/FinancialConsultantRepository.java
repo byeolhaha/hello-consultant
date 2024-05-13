618036016a93765f21518bcb6b0ff533b9f95674
byeolhaha <j35635aa@gmail.com>
2024-05-13 11:57:41 +0900

    feat : 상담원 사원번호로 검색 쿼리 구현

--- src/main/java/com/hellomeritz/member/repository/fc/FinancialConsultantRepository.java
+++ src/main/java/com/hellomeritz/member/repository/fc/FinancialConsultantRepository.java
@@ -14,4 +14,6 @@ public interface FinancialConsultantRepository {
 
     List<FinancialConsultant> getFinancialConsultantWithAvailable();
 
+    FinancialConsultant getByEmployeeNumber (String employeeNumber);
+
 }
