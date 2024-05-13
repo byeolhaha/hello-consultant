cfd161e94a783460b36d64f9eca1b88a82493b2a
byeolhaha <j35635aa@gmail.com>
2024-05-13 13:51:15 +0900

    style : 컨트롤러 어노테이션 추가

--- src/main/java/com/hellomeritz/chat/controller/BanWordController.java
+++ src/main/java/com/hellomeritz/chat/controller/BanWordController.java
@@ -5,7 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/banwords")
 public class BanWordController {
 
